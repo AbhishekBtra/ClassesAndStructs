54a435e088e05d0f08fdd672922cf2c458df8348
JustGitt <abatra002@gmail.com>
2017-11-15 15:51:45 +0530

    Equivalence

---
+++ ClassesNStructs/ClassesNStructs/BaseClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesNStructs
+{
+    class BaseClass
+    {
+        int x, y, z;
+
+        public BaseClass()
+        {
+            x = 10;
+            y = 10;
+            z = 10;
+        }
+
+        public virtual int CalcVolume()
+        {
+            int vol;
+            return vol = x*y*z;
+        }
+
+        public int CalArea()
+        {
+            return x + y * z;
+        }
+
+
+
+        public int CalArea2()
+        {
+            return x + y * z;
+        }
+
+
+
+    }
+}
